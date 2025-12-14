6b49eea71ef9e3abc7b3ad2991126ce06577d028
umer <syedumerahmedcode@gmail.com>
2025-12-14 10:46:25 +0100

    Feature: used tested components.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from "./navbar/navbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -3,7 +3,9 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-navbar',
     standalone: true,
-    template: '<h1>Navbar Component</h1>'
+    template: `<h1>Navbar Component</h1>
+                <p>Dummy paragraph</p>
+                <button>submit</button>`
 
 
 })
