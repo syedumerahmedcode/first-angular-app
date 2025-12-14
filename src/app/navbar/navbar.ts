2b8b6912994122c261d45838cd200f8f91fdbbb3
umer <syedumerahmedcode@gmail.com>
2025-12-14 10:25:34 +0100

    Feature:  Created navbar component with sample text

---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-navbar',
+    standalone: true,
+    template: '<h1>Navbar Component</h1>'
+
+
+})
+
+export class AppNavbar {
+
+}
