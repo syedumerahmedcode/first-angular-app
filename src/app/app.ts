4e832763e09e98dfc47e4765c03ac39dc4933662
umer <syedumerahmedcode@gmail.com>
2025-12-14 11:23:18 +0100

    Featre: Generating components using Angular CLI and connecting the, to the main page.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from "./navbar/navbar";
+import { Header } from './header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppNavbar],
+  imports: [RouterOutlet, AppNavbar, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
